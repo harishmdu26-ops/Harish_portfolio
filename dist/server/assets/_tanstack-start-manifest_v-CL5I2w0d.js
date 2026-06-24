91802434045c5bddb9171aec27be80d13bf81d3d
harishmdu26-ops <harishmdu26@gmail.com>
2026-06-24 19:48:22 +0530

    Add files via upload

---
+++ dist/server/assets/_tanstack-start-manifest_v-CL5I2w0d.js
@@ -0,0 +1,20 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/HP/Downloads/harish-a-pro-site-main/harish-a-pro-site-main/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-DHxsjrL7.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DHxsjrL7.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/HP/Downloads/harish-a-pro-site-main/harish-a-pro-site-main/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-cKudHu4y.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
